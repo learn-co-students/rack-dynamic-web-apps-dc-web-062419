cbab7ee1cedc409689569b26c6cac391f66df797
Luke O'Malley <Lukeomalley95@gmail.com>
2019-07-13 18:14:28 -0400

    Completed LAba🤙

--- spec/rack_mechanics_spec.rb
+++ spec/rack_mechanics_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require_relative './spec_helper'
-describe "an ok request" do
+describe 'an ok request' do
   it 'wins if they are all the same' do
     allow(Kernel).to receive(:rand).and_return(2)
     get '/'
-    expect(last_response.body).to include("You Win")
+    expect(last_response.body).to include('You Win')
   end
 
   it 'loses if they are different' do
-    numbers = [1,2,3]
+    numbers = [1, 2, 3]
     index = 0
-    allow(Kernel).to receive(:rand){
+    allow(Kernel).to receive(:rand) {
       numbers[index]
-      index+=1
+      index += 1
     }
 
     get '/'
-    expect(last_response.body).to include("You Lose")
+    expect(last_response.body).to include('You Lose')
   end
 end
